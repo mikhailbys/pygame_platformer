2edaf33e6853af1814931c231cb99671f601f3d7
mikhailbys <bystrovmikhailad@gmail.com>
2022-09-05 13:46:23 +0400

    change main cycle: add exit on game end, notify user on hero death and hero victory

--- src/main.py
+++ src/main.py
@@ -4,26 +4,35 @@ from pygame import *
 from src.config import DISPLAY, FPS
 from src.utils.level_loader import load_level
 from src.levels.level_select import level_dict
-from src.utils.screen_render import display_lives_counter, display_game_over, display_game_complete
+from src.utils.screen_render import display_lives_counter, display_game_over, display_game_complete, \
+    display_level_complete
+
+
+# check if hero still have lives
+def is_alive(hero):
+    return hero.lives_count > 0
 
 
 def main():
-    pygame.init()  # Init PyGame
-    screen = pygame.display.set_mode(DISPLAY)  # Create window
-    pygame.display.set_caption("Mario Game")  # Set window title
-    background_image = pygame.image.load('assets/background/background.jpeg')  # apply background
+    # Init PyGame
+    pygame.init()
+    # Create window
+    screen = pygame.display.set_mode(DISPLAY)
+    # Set window title
+    pygame.display.set_caption("Mario Game")
+    # apply background
+    background_image = pygame.image.load('assets/background/background.jpeg')
+    #
+    is_win = False
 
     for lvl in level_dict:
         # set up in-game modules
         camera, timer, left, right, up, hero, entities, animated_entities, monsters, platforms \
             = load_level(lvl)
-        running = True
-        # main game loop
-        while not hero.winner:  # change for while running
 
-            if hero.lives_count == 0:
-                running = False
-                display_game_over(screen)
+        # main game loop
+        # todo change for while running
+        while not hero.winner and is_alive(hero):
 
             # fps
             timer.tick(FPS)
@@ -45,7 +54,7 @@ def main():
                 if e.type == KEYUP and e.key == K_UP:
                     up = False
 
-            # updates
+            # sprite updates
             screen.blit(background_image, (0, 0))
             animated_entities.update()
             monsters.update(platforms)
@@ -62,15 +71,21 @@ def main():
             # update all and output on screen
             pygame.display.update()
 
-        # когда заканчиваем уровень
-        for e in entities:
-            screen.blit(e.image, camera.apply(e))  # еще раз все перерисовываем
-        cur_font = pygame.font.Font(None, 38)
-        text = cur_font.render(("Level complete!"), True,
-                                   (255, 255, 255))  # выводим надпись
-        screen.blit(text, (10, 100))
-        pygame.display.update()
-        time.wait(10000)
+        # on level exit
+        if is_alive(hero) > 0:
+            # on level complete
+            is_win = True
+            display_level_complete(screen)
+            time.wait(2000)
+        else:
+            is_win = False
+            display_game_over(screen)
+            time.wait(2000)
+            break
+
+    # on game end
+    display_game_complete(screen, is_win)
+    time.wait(3000)
 
 
 if __name__ == "__main__":
--- src/utils/screen_render.py
+++ src/utils/screen_render.py
@@ -26,22 +26,36 @@ def display_lives_counter(screen, lives_count):
 
 
 def display_game_over(screen):
-    time.sleep(300)
-    render_text(
-        pygame.font.SysFont("Arial", 64),
-        'GAME OVER',
-        "white",
-        (DISPLAY[0] / 2, DISPLAY[1] / 2),
-        screen
+    cur_font = pygame.font.Font(None, 38)
+    text = cur_font.render(
+        "Game Over!",
+        True,
+        (255, 255, 255),
+        # todo background
     )
+    screen.blit(text, (10, 100))
+    pygame.display.update()
 
 
-def display_game_complete(screen):
-    time.sleep(300)
-    render_text(
-        pygame.font.SysFont("Arial", 64),
-        'COMPLETE',
-        "white",
-        (DISPLAY[0] / 2, DISPLAY[1] / 2),
-        screen
+def display_game_complete(screen, is_win):
+    cur_font = pygame.font.Font(None, 38)
+    text = cur_font.render(
+        "Game Complete! Exit in 3 sec" if is_win else "Exit in 3 sec",
+        True,
+        (255, 255, 255),
+        # todo background
     )
+    screen.blit(text, (10, 100))
+    pygame.display.update()
+
+
+def display_level_complete(screen):
+    cur_font = pygame.font.Font(None, 38)
+    text = cur_font.render(
+        "Level complete!",
+        True,
+        (255, 255, 255),
+        # todo background
+    )
+    screen.blit(text, (10, 100))
+    pygame.display.update()
